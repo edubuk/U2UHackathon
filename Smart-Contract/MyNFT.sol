3a0f9d72c2be50effa78687fc7c8a77b07d8b34c
ajeetram <ajeetramverma10@gmail.com>
2025-09-23 00:04:52 +0530

    smart-contract-initialize

---
+++ Smart-Contract/MyNFT.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC721URIStorage, ERC721} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MyNFT is ERC721URIStorage{
+
+}
